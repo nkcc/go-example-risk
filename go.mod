edcb17a1b0eea42a5053dc37496b7a80b14ff769
gitlawr <lawrleegle@gmail.com>
2022-04-18 11:49:05 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module example.com/m
+
+go 1.18
+
+require golang.org/x/text v0.3.2
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"golang.org/x/text/encoding/unicode"
+)
+
+func main() {
+	fmt.Println(unicode.ErrMissingBOM)
+}
